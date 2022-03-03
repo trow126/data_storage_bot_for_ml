e8934b1575a6ca46cd515ebdef9ccd094c829c8d
trow126 <43240931+trow126@users.noreply.github.com>
2022-03-03 10:38:53 +0900

    Add files via upload

---
+++ data_storage_bot_for_ml/combinations_test.py
@@ -0,0 +1,118 @@
+import pprint as pp
+import time
+
+features = sorted([
+                        'ADX',
+                        'ADXR',
+                        'APO',
+                        'AROON_aroondown',
+                        'AROON_aroonup',
+                        'AROONOSC',
+                        'CCI',
+                        'DX',
+                        'MACD_macd',
+                        'MACD_macdsignal',
+                        'MACD_macdhist',
+                        'MFI',
+                        'MINUS_DI',
+                        'MINUS_DM',
+                        'MOM',
+                        'PLUS_DI',
+                        'PLUS_DM',
+                        'RSI',
+                        'STOCH_slowk',
+                        'STOCH_slowd',
+                        'STOCHF_fastk',
+                        'STOCHRSI_fastd',
+                        'ULTOSC',
+                        # 'WILLR',
+                        # 'ADOSC',
+                        # 'NATR',
+                        # 'HT_DCPERIOD',
+                        # 'HT_DCPHASE',
+                        # 'HT_PHASOR_inphase',
+                        # 'HT_PHASOR_quadrature',
+                        # 'HT_TRENDMODE',
+                        # 'BETA',
+                        # 'LINEARREG',
+                        # 'LINEARREG_ANGLE',
+                        # 'LINEARREG_INTERCEPT',
+                        # 'LINEARREG_SLOPE',
+                        # 'STDDEV',
+                        # 'BBANDS_upperband',
+                        # 'BBANDS_middleband',
+                        # 'BBANDS_lowerband',
+                        # 'DEMA',
+                        # 'EMA',
+                        # 'HT_TRENDLINE',
+                        # 'KAMA',
+                        # 'MA',
+                        # 'MIDPOINT',
+                        # 'T3',
+                        # 'TEMA',
+                        # 'TRIMA',
+                        # 'WMA',
+                    ])
+
+# def factorial(n):
+#     res = 1
+#     for i in range(1, n + 1):
+#         res *= i
+#     return res
+
+# def combination(n, r):
+#     return factorial(n) / (factorial(n - r) * factorial(r))
+
+# n = 8
+# total = 0
+
+# for r in range(1, n+1):
+#     res = combination(n, r)
+#     print("{n}C{r} = {res}".format(
+#         n=n,
+#         r=r,
+#         res=res))
+#     total += res
+
+# print("合計:", total)
+# num_list = list(range(1, 9))
+
+
+start = time.perf_counter()
+
+
+def nCr(s1, r):  # ただしmは、1 <= m <= len(s1) の範囲とする
+
+    a = s1[0]  # 配列の最初の値を対象する
+    # a と組み合わせられる値は aを以外の配列を対象とすれば良い
+    s2 = s1[1:]  # => [2, 3, 4, 5]
+
+    res = []
+    if r == 1:  # 例外ケースとして処理
+        for a in s1:
+            res.append(a)
+        return res
+
+    elif r == 2:
+        for b in s2:
+            res.append([a, b])
+    else:
+        for bc in nCr(s2, r-1):
+            # bc.insert(0, a)
+            bc.append(a)
+            res.append(bc)
+
+    if len(s1) > r - 1:
+        return res + nCr(s2, r)
+    else:
+        return []
+
+
+
+for i in range(1, len(features)+1):
+    # print(len(nCr(features, i)))
+    nCr(features, i)
+
+
+end = time.perf_counter()
+print("実行時間:",end - start)
---
+++ data_storage_bot_for_ml/combinations_test_cache.py
@@ -0,0 +1,140 @@
+import pprint as pp
+import time
+import itertools
+import csv
+import pandas as pd
+import numpy as np 
+
+features = sorted([
+                    'ADX',
+                    'ADXR',
+                    'APO',
+                    'AROON_aroondown',
+                    'AROON_aroonup',
+                    'AROONOSC',
+                    'CCI',
+                    'DX',
+                    # 'MACD_macd',
+                    # 'MACD_macdsignal',
+                    # 'MACD_macdhist',
+                    # 'MFI',
+                    # 'MINUS_DI',
+                    # 'MINUS_DM',
+                    # 'MOM',
+                    # 'PLUS_DI',
+                    # 'PLUS_DM',
+                    # 'RSI',
+                    # 'STOCH_slowk',
+                    # 'STOCH_slowd',
+                    # 'STOCHF_fastk',
+                    # 'STOCHRSI_fastd',
+                    # 'ULTOSC',
+                    # 'WILLR',
+                    # 'ADOSC',
+                    # 'NATR',
+                    # 'HT_DCPERIOD',
+                    # 'HT_DCPHASE',
+                    # 'HT_PHASOR_inphase',
+                    # 'HT_PHASOR_quadrature',
+                    # 'HT_TRENDMODE',
+                    # 'BETA',
+                    # 'LINEARREG',
+                    # 'LINEARREG_ANGLE',
+                    # 'LINEARREG_INTERCEPT',
+                    # 'LINEARREG_SLOPE',
+                    # 'STDDEV',
+                    # 'BBANDS_upperband',
+                    # 'BBANDS_middleband',
+                    # 'BBANDS_lowerband',
+                    # 'DEMA',
+                    # 'EMA',
+                    # 'HT_TRENDLINE',
+                    # 'KAMA',
+                    # 'MA',
+                    # 'MIDPOINT',
+                    # 'T3',
+                    # 'TEMA',
+                    # 'TRIMA',
+                    # 'WMA',
+                ])
+
+def factorial(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+def combination(n, r):
+    return factorial(n) / (factorial(n - r) * factorial(r))
+
+n = 8
+total = 0
+
+# for r in range(1, n+1):
+#     res = combination(n, r)
+#     print("{n}C{r} = {res}".format(
+#         n=n,
+#         r=r,
+#         res=res))
+#     total += res
+
+# print("合計:", total)
+num_list = list(range(1, 9))
+
+
+start = time.perf_counter()
+
+cache = {}
+def join(a):
+    s = ""
+    for b in a:
+        s += str(b) + "_"
+    return s
+
+def nCr(s1, r):  # ただしmは、1 <= m <= len(s1) の範囲とする
+
+    a = s1[0]  # 配列の最初の値を対象する
+    # a と組み合わせられる値は aを以外の配列を対象とすれば良い
+    s2 = s1[1:]  # => [2, 3, 4, 5]
+
+    res = []
+    if r == 1:  # 例外ケースとして処理
+        for a in s1:
+            res.append(a)
+        return res
+
+    elif r == 2:
+        for b in s2:
+            res.append([a, b])
+    else:
+        for bc in nCr(s2, r-1):
+            # bc.insert(0, a)
+            bc.append(a)
+            res.append(bc)
+
+    if len(s1) > r - 1:
+        prefix = join(s2)
+        key = "{prefix}{n}C{r}".format(prefix=prefix, n=len(s2), r=r)
+
+        if cache.get(key) is None:
+            cache[key] = nCr(s2, r)
+
+        return res + cache[key]
+    else:
+        return []
+
+tmp_list =[]
+
+for i in range(1, len(features)+1):
+    # print((nCr(features, i)))
+    tmp_list.append(nCr(features, i))
+
+    if len(tmp_list) % 3 == 0:
+
+        with open('studentgrades.csv', mode='a', newline='') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            writer.writerows(list(tmp_list))
+            tmp_list = []
+
+end = time.perf_counter()
+print("実行時間:",end - start)
